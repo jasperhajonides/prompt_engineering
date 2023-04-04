b8a5d004a86d0e075f7f9ade71dd43541fbe36c2
Jasper Hajonides van der Meulen <38157436+jasperhajonides@users.noreply.github.com>
2023-04-05 00:00:13 +0100

    Add popup window but without the ability to add new prompts yet

---
+++ static/scripts_popup2.js
@@ -0,0 +1,127 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const submitPromptBtn = document.getElementById("submit-prompt-btn");
+  const popup = document.getElementById("popup");
+  const close = document.getElementById("close");
+  const selectTopicButton = document.getElementById("selectTopicButton");
+  const topicDropdown = document.getElementById("topicDropdown");
+  const topicOptions = document.getElementsByClassName("topicOption");
+
+  submitPromptBtn.addEventListener("click", function () {
+    popup.classList.remove("hidden");
+  });
+
+  close.addEventListener("click", function () {
+    popup.classList.add("hidden");
+  });
+
+  window.addEventListener("click", function (event) {
+    if (event.target === popup) {
+      popup.classList.add("hidden");
+    }
+  });
+
+  selectTopicButton.addEventListener("click", function () {
+    topicDropdown.classList.toggle("hidden");
+  });
+
+  async function loadTopicOptions() {
+    try {
+      const response = await fetch("http://localhost:3000/data");
+      const data = await response.json();
+      const keys = Object.keys(data);
+
+      keys.forEach((key) => {
+        const option = document.createElement("a");
+        option.href = "#";
+        option.textContent = key;
+        option.classList.add("topicOption");
+        topicDropdown.appendChild(option);
+      });
+
+      Array.from(topicOptions).forEach((option) => {
+        option.addEventListener("click", (event) => {
+          event.preventDefault();
+          selectTopicButton.textContent = event.target.textContent;
+          topicDropdown.classList.add("hidden");
+        });
+      });
+
+    } catch (error) {
+      console.error("Failed to fetch topics:", error);
+    }
+  }
+
+  const submitBtn = document.getElementById("submitBtn");
+  const subtopicInput = document.getElementById("subtopicInput");
+  const promptInput = document.getElementById("promptInput");
+
+submitBtn.addEventListener("click", function () {
+  const topic = selectTopicButton.textContent;
+  const subtopic = subtopicInput.value.trim();
+  const message = promptInput.value.trim();
+   console.log(topic)
+  if (
+    topic &&
+    topic !== "Select Topic" &&
+    subtopic &&
+    message
+  ) {
+    submitData(topic, subtopic, message);
+    popup.classList.add("hidden"); // Add this line to close the popup
+  } else {
+    alert("Please fill in all the fields before submitting.");
+  }
+});
+
+  loadTopicOptions();
+});
+
+
+/* load topics from json */
+async function loadTopicOptions() {
+  try {
+    const response = await fetch("http://localhost:3000/data");
+    const data = await response.json();
+    const keys = Object.keys(data);
+
+    keys.forEach((key) => {
+      const option = document.createElement("a");
+      option.href = "#";
+      option.textContent = key;
+      option.classList.add("topicOption");
+      topicDropdown.appendChild(option);
+    });
+
+    Array.from(topicOptions).forEach((option) => {
+      option.addEventListener("click", (event) => {
+        event.preventDefault();
+        selectTopicButton.textContent = event.target.textContent;
+        topicDropdown.classList.add("hidden");
+      });
+    });
+
+  } catch (error) {
+    console.error("Failed to fetch topics:", error);
+  }
+}
+
+/* add new data */
+async function submitData(topic, subtopic, message) {
+  try {
+    const response = await fetch(`http://localhost:3000/data/${topic}/${subtopic}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ message }),
+    });
+
+    if (response.ok) {
+      console.log("Message submitted successfully");
+    } else {
+      console.error("Failed to submit message:", response.statusText);
+    }
+  } catch (error) {
+    console.error("Failed to submit message:", error);
+  }
+}
