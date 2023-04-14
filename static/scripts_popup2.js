2f5b1e23ff6411ebbdfb8943cf69d34dfa7f63d6
Jasper Hajonides van der Meulen <38157436+jasperhajonides@users.noreply.github.com>
2023-04-14 16:09:43 +0100

    update

--- static/scripts_popup2.js
+++ static/scripts_popup2.js
@@ -24,6 +24,32 @@ document.addEventListener("DOMContentLoaded", function () {
     topicDropdown.classList.toggle("hidden");
   });
 
+  const submitBtn = document.getElementById("submitBtn");
+  const subtopicInput = document.getElementById("subtopicInput");
+  const promptInput = document.getElementById("promptInput");
+
+  submitBtn.addEventListener("click", function () {
+    const topic = selectTopicButton.textContent;
+    const subtopic = subtopicInput.value.trim();
+    const message = promptInput.value.trim();
+
+    if (
+      topic &&
+      topic !== "Select Topic" &&
+      subtopic &&
+      message
+    ) {
+      submitData(topic, subtopic, message);
+      popup.classList.add("hidden");
+    } else {
+      alert("Please fill in all the fields before submitting.");
+    }
+  });
+
+  loadTopicOptions();
+});
+
+/* load topics from json */
 async function loadTopicOptions() {
   try {
     const response = await fetch("http://localhost:3000/data");
@@ -38,62 +64,7 @@ async function loadTopicOptions() {
       topicDropdown.appendChild(option);
     });
 
-    Array.from(topicOptions).forEach((option) => {
-      option.addEventListener("click", (event) => {
-        event.preventDefault();
-        selectTopicButton.textContent = event.target.textContent;
-        topicDropdown.classList.add("hidden");
-      });
-    });
-
-  } catch (error) {
-    console.error("Failed to fetch topics:", error);
-  }
-}
-
-  const submitBtn = document.getElementById("submitBtn");
-  const subtopicInput = document.getElementById("subtopicInput");
-  const promptInput = document.getElementById("promptInput");
-
-submitBtn.addEventListener("click", function () {
-  const topic = selectTopicButton.textContent;
-  const subtopic = subtopicInput.value.trim();
-  const message = promptInput.value.trim();
-   console.log(topic)
-   console.log(subtopic)
-   console.log(message)
-
-  if (
-    topic &&
-    topic !== "Select Topic" &&
-    subtopic &&
-    message
-  ) {
-    submitData(topic, subtopic, message);
-    popup.classList.add("hidden"); // Add this line to close the popup
-  } else {
-    alert("Please fill in all the fields before submitting.");
-  }
-});
-
-  loadTopicOptions();
-});
-
-
-/* load topics from json */
-async function loadTopicOptions() {
-  try {
-    const response = await fetch("http://localhost:3000/data");
-    const data = await response.json();
-    const keys = Object.keys(data);
-
-    keys.forEach((key) => {
-      const option = document.createElement("a");
-      option.href = "#";
-      option.textContent = key;
-      option.classList.add("topicOption");
-      topicDropdown.appendChild(option);
-    });
+    const topicOptions = document.getElementsByClassName("topicOption"); // Move this line inside loadTopicOptions
 
     Array.from(topicOptions).forEach((option) => {
       option.addEventListener("click", (event) => {
@@ -111,12 +82,21 @@ async function loadTopicOptions() {
 /* add new data */
 async function submitData(topic, subtopic, message) {
   try {
+    const newEntry = {
+      topic,
+      subtopic,
+      message,
+      score: 0,
+      usage: 0,
+    };
+
+    console.log()
     const response = await fetch("http://localhost:3000/data", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({ topic, subtopic, message }),
+      body: JSON.stringify(newEntry),
     });
 
     if (response.ok) {
@@ -128,3 +108,9 @@ async function submitData(topic, subtopic, message) {
     console.error("Failed to submit message:", error);
   }
 }
+
+
+
+
+
+
